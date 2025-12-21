95bb9672f6d551edb1f85165c0671ce037c77ff8
David Liang <friedricesushi020@gmail.com>
2025-12-21 01:42:25 -0800

    add dice selector

--- src/Dice.tsx
+++ src/Dice.tsx
@@ -1,7 +1,8 @@
+import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 import { NumberInput } from './NumericInput';
 
 
-import { Dice1, Dice2, Dice3, Dices } from 'lucide-react'
+import { Dice1, Dice2, Dice3, Dice4, Dice5, Dice6, Dices } from 'lucide-react'
 
 
 type DiceSelectorProps = {
@@ -37,12 +38,37 @@ export function DiceSelector({
         <>
             <div className="flex flex-col gap-1">
                 <div className="flex items-center gap-2">
-                    {icon}
                     {label}
+                </div>
+                <div>
+                    <ToggleGroup type="single" size="sm" value={value.toString()} variant={"outline"}
+                        onValueChange={(value) => {
+                            onChange(Number(value))
+                        }}>
+                        <ToggleGroupItem value={"1"} aria-label="Toggle 1">
+                            <Dice1 />
+                        </ToggleGroupItem>
+                        <ToggleGroupItem value={"2"} aria-label="Toggle 2">
+                            <Dice2 />
+                        </ToggleGroupItem>
+                        <ToggleGroupItem value={"3"} aria-label="Toggle 3">
+                            <Dice3 />
+                        </ToggleGroupItem>
+                        <ToggleGroupItem value={"4"} aria-label="Toggle 4">
+                            <Dice4 />
+                        </ToggleGroupItem>
+                        <ToggleGroupItem value={"5"} aria-label="Toggle 5">
+                            <Dice5 />
+                        </ToggleGroupItem>
+                        <ToggleGroupItem value={"6"} aria-label="Toggle 6">
+                            <Dice6 />
+                        </ToggleGroupItem>
+
+                    </ToggleGroup>
                 </div>
                 <NumberInput min={1} max={6} placeholder={label} defaultValue={1} value={value} onValueChange={onChange} displayButtons={false} />
 
-            </div>
+            </div >
 
         </>
     )
